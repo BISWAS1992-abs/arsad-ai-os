0ae24c68b46d7adfdfb0c3d69a2d835e3b4431e8
BISWAS1992-abs <arsadul192@gmail.com>
2026-07-04 01:04:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
 """
 Arsad AI OS
 Main Entry Point
-Version: 1.0
 """
 
-from app import app
-
-
-def main():
-    print("===================================")
-    print("🚀 Arsad AI OS Starting...")
-    print("===================================")
-
-    app.run(host="0.0.0.0", port=5000)
-
+from bot import main
 
 if __name__ == "__main__":
     main()
